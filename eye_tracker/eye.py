e04a17cfcc80433355de59d954eecbc53678af2b
shevchenkokk <kirill_07_02@mail.ru>
2023-07-31 18:58:09 +0600

    code description completed

--- eye_tracker/eye.py
+++ eye_tracker/eye.py
@@ -3,14 +3,26 @@ import numpy as np
 import math
 
 class Eye:
+    '''
+    Класс для представления глаза на изображении с лицом, содержащий
+    методы для анализа и определения характеристик глаза
+    '''
+
     # индексы ключевых точек для левого и правого глаза
     left_eye_indices = [36, 37, 38, 39, 40, 41]
     right_eye_indices = [42, 43, 44, 45, 46, 47]
 
     def __init__(self, frame, face_shape, is_left_eye):
+        # текущий кадр
         self.frame = frame
+        
+        # набор точек, представляющих форму лица
         self.face_shape = face_shape
+        
+        # соответствует ли экземпляр класса левому глазу
         self.is_left_eye = is_left_eye
+        
+        # анализ текущего кадра
         self._analyze_frame()
 
     def _analyze_frame(self):
@@ -36,14 +48,16 @@ class Eye:
                 (0, 255, 0),
                 1,
             )
-
+    
     @staticmethod
     def _find_dist(vec1, vec2):
+        # определение расстояния между ключевыми точками
         return math.sqrt(
              math.pow(vec1[0] - vec2[0], 2) + math.pow(vec1[1] - vec2[1], 2)
         )
 
     def _get_eye_aspect(self):
+        # вычисление отношения между шириной и высотой глаза
         v_dist_first = self._find_dist(self.eye_points[1], self.eye_points[-1])
         v_dist_second = self._find_dist(self.eye_points[2], self.eye_points[-2])
         h_dist = self._find_dist(self.eye_points[0], self.eye_points[-3])
@@ -53,11 +67,13 @@ class Eye:
         return self.eye_aspect
 
     def _get_gaze_relation(self):
+        # создание маски изображения размером с кадр
         mask = np.zeros(self.frame.shape[:2], dtype=np.uint8)
+        # отрисовка и заполнение внутренностей контуров глаза на маске
         cv2.polylines(mask, [np.array(self.eye_points)], True, 255, 2)
         cv2.fillPoly(mask, [np.array(self.eye_points)], 255)
+        # применение маски к текущему кадру
         eye_frame = cv2.bitwise_and(self.frame, self.frame, mask=mask)
-
 
         # нахождение минимальных и максимальных координат глаза
         min_x = min(self.eye_points[i][0] for i in range(len(self.eye_points)))
@@ -68,20 +84,26 @@ class Eye:
         # вырезка области с глазом
         eye_frame = eye_frame[min_y: max_y, min_x: max_x]
         gray_eye_frame = cv2.cvtColor(eye_frame, cv2.COLOR_BGR2GRAY)
+
+        # фильтрация изображения для сглаживания шумов
         gray_eye_frame = cv2.GaussianBlur(gray_eye_frame, (5, 5), 0)
         gray_eye_frame = cv2.medianBlur(gray_eye_frame, 5)
 
+        # бинаризация изображения
         _, threshold = cv2.threshold(gray_eye_frame, 80, 255, cv2.THRESH_BINARY)
         threshold = cv2.resize(threshold, None, fx=5, fy=5)
         
         height, width = threshold.shape[:2]
         
+        # разделение изображения на области, соответствующие левой и правой части глаза
         left_side_threshold = threshold[0: height, 0: int(width / 2)]
         right_side_threshold = threshold[0: height, int(width / 2): width]
         
+        #подсчёт количества белых пикселей на левой и правой части глаза
         left_side_white = cv2.countNonZero(left_side_threshold)
         right_side_white = cv2.countNonZero(right_side_threshold)
 
+        # обработка частных случаев
         match left_side_white, right_side_white:
             case 0, _:
                 self.gaze_relation = 1
